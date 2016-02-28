0847facabfc9ef55fa59244cef4ef3e2998a7100
kemalgafar <kemalgafar@gmail.com>
2016-02-28 10:22:55 -0500

    Added files via upload

--- gameMechanics.py
+++ gameMechanics.py
@@ -1,4 +1,4 @@
-import checkersBoard as ckbd
+from check import Board
 import re
 
 #need a check space func
@@ -9,8 +9,8 @@ charLookUp = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7}
 
 def userInput():
     print "Please enter in a coordinate you would like to move to:"
-    userString = raw_input(">  ")
-    return userString
+    user_string = raw_input(">  ")
+    return user_string
 
 def checkInput():
     str = userInput().upper()
@@ -24,15 +24,19 @@ def checkInput():
 def assignValues():
     """col and row swapped need to switch then assign"""
     #use a while loop?
-    while bad_Input = True:
-        userInputVals = checkInput()
-        bad_Input = checkValidMove(userInputVals)
+    while bad_input = True:
+    
+        user_input_vals = checkInput()
         
-    userMoveCoords[0] = int(userInputVals[1]) - 1
-    userMoveCoords[1] = charLookUp[userInputVals[0]]
-    userMoveCoords[2] = int(userInputVals[3]) - 1
-    userMoveCoords[3] = charLookUp[userInputVals[2]]
-    return userMoveCoords
+        user_move_coords = []
+        user_move_coords[0] = int(user_input_vals[1]) - 1
+        user_move_coords[1] = charLookUp[user_input_vals[0]]
+        user_move_coords[2] = int(user_input_vals[3]) - 1
+        user_move_coords[3] = charLookUp[user_input_vals[2]]
+        
+        bad_input = checkValidMove(user_move_coords)
+        
+    return user_move_coords
 
 # needs a func tocheck if inp is valid beforehand
 
@@ -43,15 +47,27 @@ def checkIfLegal():
     if gameBoard[coordCheck[0]][coordCheck[1]].isAlive() == True
 """
 
-def checkValidMove(userInputVals):
-    """"""
     #need to check if there is a peice selected on the sq  feedbk" err inp sel sq with piece
     #need to check if there is a piece that has moves avail (weights) select greatest? feedbk" follow rules, if move avail must take
     #need to force user (have the abv func return a list, need to pick from the list) to move it to the right spot
     #NEED A GEERERIC func to return the type of a 1/1 sq so that can use to either move or get type to eat
     #if cant eat then need to check if spce is free, then call move
     #need to check if king then recursively do the above
-    if 
+    # need a func that destroys a eaten piece
+
+
+def checkValidMove(user_move_coords): #has to return a bool
+    """"""
+    # need a func to return the # of pieces tat have possible moves
+    # append all alive pieces to an array, make the array 2D? if 0 moves then array is empty
+    # if 1 move then [1][append1, append2...] , [2]
+    tst_arr = user_move_coords #only to save space/improve readabilty 
+    if tst_arr[0], tst_arr[1] == tst_arr[2], tst_arr[3]:
+        print "test case 1"
+    elif Board.cur_state[tst_arr[0]][tst_arr[1]] == None:
+        print "tst case 2"
+    elif
+    
 
 
 
