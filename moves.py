43899b23a20ba3e3c43bb86a7174ee6961b80a40
Kemal Gafar <kemalgafar@gmail.com>
2016-05-01 23:13:24 -0400

    Added files via upload
    Made good progress with processing the movement string/tree, need to debug final append to string

--- moves.py
+++ moves.py
@@ -17,6 +17,16 @@ def place_pieces(cur_state):
     """Places pieces on a game board at the starting positions"""
     initial_board = cur_state
 
+    initial_board[0][3] = "R"
+    initial_board[1][2] = "B"
+    initial_board[1][4] = "B"
+    initial_board[3][4] = "B"
+    initial_board[3][6] = "B"
+    initial_board[5][2] = "B"
+
+    cur_state = initial_board
+
+    """
     initial_board[0][1] = "R"
     initial_board[0][3] = "R"
     initial_board[0][5] = "R"
@@ -35,23 +45,18 @@ def place_pieces(cur_state):
     initial_board[5][6] = "B"
     initial_board[6][1] = "B"
     initial_board[6][3] = "B"
-    #initial_board[6][5] = "B"
+    initial_board[6][5] = "B"
     initial_board[6][7] = "B"
     initial_board[7][0] = "B"
     initial_board[7][2] = "B"
     initial_board[7][4] = "B"
     initial_board[7][6] = "B"
-
-    cur_state = initial_board
+    """
 
 place_pieces(cur_state)
 
-
-print cur_state
-
 def possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str):
 
-    move_str = hist_str
     # need to get a recursive function to TREE through the spaces
     # only use first 2 chars from hist string that way u can keep adding onto the string
 
@@ -78,58 +83,68 @@ def possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str):
         self_2 = "KB"
 
 
-'''
-make each of the 4 choices a individ func
-have logic (if else) control
-if red or is_king do func1 func2
-else if ....
-each of the 4 possibilites is a func onto itself and calls possible moves again
-func(+1,+1)
-func(+1,-1)
-func(-1, +1)
-func(-1,-1)
-'''
+    '''
+    make each of the 4 choices a individ func
+    have logic (if else) control
+    if red or is_king do func1 func2
+    else if ....
+    '''
+
+    new_i = i
+    new_j = j
 #handle negative numbers, so you dont appened a negative # to the string (move not valid)
 #or have a regex to handle when deciding which string to use
-    if cur_state[i][j] == self_1 or self_2:
+#make 4 funcs  need to call at the same time/ i/j will change, before call of func can check is king?
+    try:
+        move_p1_p1(cur_state, new_i, new_j, valid_spaces, hist_str, move_str, enemy_1, enemy_2)
+        move_p1_m1(cur_state, new_i, new_j, valid_spaces, hist_str, move_str, enemy_1, enemy_2)
+    except IndexError:
+        pass
+
+def move_p1_p1(cur_state, new_i, new_j, valid_spaces, hist_str, move_str, enemy_1, enemy_2):
+    if cur_state[new_i+1][new_j+1] == enemy_1 or enemy_2:
         try:
-            if cur_state[i+1][j+1] == enemy_1 or enemy_2:
-                try:
-                    if cur_state[i+2][j+2] == None:
-                        move_str += (str(i+2) + str(j+2))
-                        valid_spaces[hist_str].append(move_str)
-                        i += 2
-                        j += 2
-                        cur_state[i][j] = self_1
-                        possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str) #need to return?
-                except IndexError:
-                    pass
-            if cur_state[i+1][j-1] == enemy_1 or enemy_2:   #handle the negative here?
-                try:
-                    if cur_state[i+2][j-2] == None:
-                        move_str += (str(i+2) + str(j-2))
-                        valid_spaces[hist_str].append(move_str)
-                        i += 2
-                        j -= 2
-                        cur_state[i][j] = self_1
-                        possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str) #need to return?
-                except IndexError:
-                    pass
+            if cur_state[new_i+2][new_j+2] == None:
+                new_i += 2
+                new_j += 2
+                if (new_i < 8) and (new_j < 8): #each of the four will be diff
+                    move_str += (str(new_i) + str(new_j))
+                    valid_spaces[hist_str].append(move_str)
+                    i = new_i
+                    j = new_j
+                    possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str) #need to return?
         except IndexError:
             pass
 
-#NEED 4 of them
+def move_p1_m1(cur_state, new_i, new_j, valid_spaces, hist_str, move_str, enemy_1, enemy_2):
+    if cur_state[new_i+1][new_j-1] == enemy_1 or enemy_2:
+        try:
+            if cur_state[new_i+2][new_j-2] == None:
+                new_i += 2
+                new_j -= 2
+                if (new_i < 8) and (new_j > -1):
+                    move_str += (str(new_i) + str(new_j))
+                    valid_spaces[hist_str].append(move_str)
+                    i = new_i
+                    j = new_j
+                    possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str) #need to return?
+        except IndexError:
+            pass
+
 
 def iterate_board(cur_state, valid_spaces):
         # have this function be called twice per turn (1 red 1 black)
         # have the enemy state keep changing in the board class
+        # maybe pass in a tuple? a 2 member list? etc..
     for i in xrange(8):
         for j in xrange(8):
             #append the lower func?
             hist_str = (str(i) + str(j))
-            if hist_str in valid_spaces:
-                move_str = ""  #is passing this in nessecary
+            if hist_str in valid_spaces and cur_state[i][j] == "R":
+                move_str = hist_str  #is passing this in nessecary
                 possible_moves(cur_state, i, j, valid_spaces, hist_str, move_str)
+
+#def is_red_turn():
 
 iterate_board(cur_state, valid_spaces)
 print valid_spaces
